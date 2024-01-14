6def0e3ef0debd399824a4b60f806040d071efb6
Lucas Saladini <lucassaladini@hotmail.com>
2024-01-14 11:59:55 -0300

    Working with properties, methods and constructors

---
+++ Propriedades_Metodos_Construtores/Program.cs
@@ -0,0 +1,23 @@
+﻿using Propriedades_Metodos_Construtores.Models;
+
+Pessoa p1 = new Pessoa(nome: "Lucas", sobrenome: "Saladini");
+
+Pessoa p2 = new Pessoa(nome: "Leonardo", sobrenome: "Neves Queiroz");
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Inglês";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
+
+
+
+
+
+
+
+
+// p1.Idade = 30;
+// p1.Apresentar();
