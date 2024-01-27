f96a90bdc72f8da841d7c9d08fe9833804975db7
Lucas Saladini <lucassaladini@hotmail.com>
2024-01-27 11:07:52 -0300

    Working with abstraction and encapsuling

---
+++ Abstracao_Encapsulamento/Program.cs
@@ -0,0 +1,12 @@
+﻿using Abstracao_Encapsulamento.Models;
+
+Pessoa p1 = new Pessoa();
+
+p1.Nome = "Lucas";
+p1.Idade = 30;
+p1.Apresentar();
+
+ContaCorrente c1 = new ContaCorrente(123, 1000);
+c1.ExibirSaldo();
+c1.Sacar(5000);
+c1.ExibirSaldo();
