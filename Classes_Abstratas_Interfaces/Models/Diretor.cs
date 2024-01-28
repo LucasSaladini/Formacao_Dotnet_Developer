4239da8e1f6a9a81abe14d0d54dd7ef96e61e145
Lucas Saladini <lucassaladini@hotmail.com>
2024-01-28 17:56:27 -0300

    Working with abstract classes and interfaces

---
+++ Classes_Abstratas_Interfaces/Models/Diretor.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// namespace Classes_Abstratas_Interfaces.Models
+// {
+//     public class Diretor : Professor
+//     {
+//         public override void Apresentar()
+//         {
+//             Console.WriteLine("Diretor");
+//         }
+//     }
+// }
---
+++ Classes_Abstratas_Interfaces/Program.cs
@@ -0,0 +1,17 @@
+﻿using Classes_Abstratas_Interfaces.Interfaces;
+using Classes_Abstratas_Interfaces.Models;
+
+Corrente conta = new Corrente();
+conta.Creditar(500);
+conta.ExibirSaldo();
+
+Pessoa p1 = new Pessoa("Lucas");
+p1.Apresentar();
+Aluno a1 = new Aluno("Eduardo");
+a1.Apresentar();
+
+Computador c = new Computador();
+Console.WriteLine(c.ToString());
+
+ICalculadora calc = new Calculadora();
+Console.WriteLine(calc.Multiplicar(3, 9));
