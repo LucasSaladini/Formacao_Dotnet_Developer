930124afda704ab72997af0f258d60e35f4262b6
Lucas Saladini <lucassaladini@hotmail.com>
2024-01-28 16:19:26 -0300

    Heranças e Polimorfismos

---
+++ Heranca_Polimorfirmos/Program.cs
@@ -0,0 +1,13 @@
+﻿using Heranca_Polimorfirmos.Models;
+
+Aluno a1 = new Aluno();
+a1.Nome = "Lucas";
+a1.Idade = 20;
+a1.Nota = 10;
+a1.Apresentar();
+
+Professor p1 = new Professor();
+p1.Nome = "Patricia";
+p1.Idade = 30;
+p1.Salario = 1000;
+p1.Apresentar();
