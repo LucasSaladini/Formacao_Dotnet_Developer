d24bfa64e23a36312549ec469ce1be223f75e601
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2024-02-28 11:23:32 +0000

    setup environment to use Azure Tables

---
+++ Azure_Table/Controllers/ContatoController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microfsoft.ApsNetCore.Mvc;
+
+namespace Azure_Table.Controllers
+{
+    [ApiController]
+    [Route("(controller)")]
+    public class ContatoController : ControllerBase
+    {
+
+    }
+}
