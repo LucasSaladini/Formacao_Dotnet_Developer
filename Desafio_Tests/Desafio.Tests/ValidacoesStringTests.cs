e95e43df577c803a17b7fef92146e04e858bbc28
Lucas Saladini <lucassaladini@hotmail.com>
2024-03-03 18:04:22 -0300

    Implemented tests for ValidacoesString

--- Desafio_Tests/Desafio.Tests/ValidacoesStringTests.cs
+++ Desafio_Tests/Desafio.Tests/ValidacoesStringTests.cs
@@ -2,16 +2,60 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Desafio.Console.Services;
 using Xunit;
 
 namespace Desafio.Tests
 {
     public class ValidacoesStringTests
     {
+        private ValidacoesString _validacoes = new ValidacoesString();
         [Fact]
-        public void Test1()
+        public void DeveRetornar6QuantidadeCaracteresDaPalavraMatrix()
         {
-            Assert.True(true);
+            //Arrange
+            var texto = "Matrix";
+            var resultadoEsperado = 6;
+            //Act
+            var resultado = _validacoes.RetornarQuantidadeCaracteres(texto);
+            //Assert
+            Assert.Equal(resultadoEsperado, resultado);
+        }
+
+        [Fact]
+        public void DeveConterAPalavraQualquerNoTexto()
+        {
+            //Arrange
+            var texto = "Esse é um texto qualquer";
+            var textoProcurado = "qualquer";
+            //Act
+            var resultadoEsperado = _validacoes.ContemCaractere(texto, textoProcurado);
+            //Assert
+            Assert.True(resultadoEsperado);
+        }
+
+        [Fact]
+        public void NaoDeveContarAPalavraTesteNoTexto()
+        {
+            // Arrange
+            var texto = "Esse é um texto qualquer";
+            var textoProcurado = "teste";
+            // Act
+            var resultado = _validacoes.ContemCaractere(texto, textoProcurado);
+            // Assert
+            Assert.False(resultado);
+        }
+
+        [Fact]
+        public void TextoDeveTerminarComAPalavraProcurado()
+        {
+            // Arrange
+            var texto = "Começo, meio e fim do texto procurado";
+            var textoProcurado = "procurado";
+            // Act
+            var resultado = _validacoes.TextoTerminaCom(texto, textoProcurado);
+            // Assert
+            Assert.True(resultado);
         }
     }
 }
